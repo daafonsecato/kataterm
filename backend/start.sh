f974f9c9d7c08d13c47a118362232e78da935d57
David Fonseca <david.fonseca@endava.com>
2024-01-25 07:18:20 +0000

    Added docker in docker for backend and all running with docker compose

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-useradd git-katas-user -m
-ttyd -p 7681 -i 0.0.0.0 --writable -w /home/git-katas-user bash &
-code-server --auth none --bind-addr 0.0.0.0:8080 /home/git-katas-user &
 cd /app
 air 
